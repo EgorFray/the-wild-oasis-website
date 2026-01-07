c4c2a9e9575637f2fe0b93353caa0a0e4de56e5d
EgorFray <fatamorganaa933@gmail.com>
2026-01-07 18:11:06 +0200

    Data fetching

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,12 @@
-export default function Page() {
-	return <h1>Cabins page</h1>;
+export default async function Page() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+	return (
+		<div>
+			<h1>Cabins page</h1>
+			{data.map((user) => (
+				<li key={user.id}>{user.name}</li>
+			))}
+		</div>
+	);
 }
