80bc92197e1e37dfc207c3db5465f0be78037c67
EgorFray <fatamorganaa933@gmail.com>
2026-01-16 12:03:30 +0200

    Make current filter active

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -7,7 +7,9 @@ function Filter() {
 	const router = useRouter();
 	const pathname = usePathname();
 
-	function handleClick(filter) {
+	const activeFilter = searchParams.get("capacity") ?? "all";
+
+	function handleFilter(filter) {
 		const params = new URLSearchParams(searchParams);
 		params.set("capacity", filter);
 		router.replace(`${pathname}?${params.toString()}`, { scroll: false });
@@ -15,32 +17,37 @@ function Filter() {
 
 	return (
 		<div className="border border-primary-800 flex">
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleClick("all")}
-			>
+			<Button filter="all" handleFilter={handleFilter} activeFilter={activeFilter}>
 				All cabins
-			</button>
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleClick("small")}
-			>
+			</Button>
+			<Button filter="small" handleFilter={handleFilter} activeFilter={activeFilter}>
 				1&mdash;3 guests
-			</button>
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleClick("medium")}
+			</Button>
+			<Button
+				filter="medium"
+				handleFilter={handleFilter}
+				activeFilter={activeFilter}
 			>
 				4&mdash;7 guests
-			</button>
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleClick("large")}
-			>
+			</Button>
+			<Button filter="large" handleFilter={handleFilter} activeFilter={activeFilter}>
 				8&mdash;12 guests
-			</button>
+			</Button>
 		</div>
 	);
+
+	function Button({ filter, handleFilter, activeFilter, children }) {
+		return (
+			<button
+				className={`px-5 py-2 hover:bg-primary-700 ${
+					filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+				}`}
+				onClick={() => handleFilter(filter)}
+			>
+				{children}
+			</button>
+		);
+	}
 }
 
 export default Filter;
