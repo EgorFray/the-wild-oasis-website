f023a7705e2f849200d116b3027f63bf44298f8b
EgorFray <fatamorganaa933@gmail.com>
2026-01-22 15:29:07 +0200

    Protecting route using middleware

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,6 +8,12 @@ const authConfig = {
 			clientSecret: process.env.AUTH_GOOGLE_SECRET,
 		}),
 	],
+	callbacks : {
+		authorized(auth, request) {
+			// This is to fast convert any type to bool
+			return !!auth?.user
+		};
+	}
 };
 
 export const { handlers, auth } = NextAuth(authConfig);
