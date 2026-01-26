b1146650f870e6ec4b081d7549c0690e81a99b7c
EgorFray <fatamorganaa933@gmail.com>
2026-01-26 14:35:39 +0200

    manual cache revalidation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -19,6 +20,8 @@ export async function updateGuest(formData) {
 		.eq("id", sessionStorage.user.guestId);
 
 	if (error) throw new Error("Guest could not be updated");
+
+	revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
